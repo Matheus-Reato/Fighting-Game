5c0ba0b8ef7868c8d7a8a2c8dcf39906a45cf016
Gabriel Baldez <130607246+GabrielBBaldez@users.noreply.github.com>
2024-10-15 19:21:51 -0300

    Jump update

--- js/classes.js
+++ js/classes.js
@@ -98,6 +98,7 @@ class Fighter extends Sprite {
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height - 95) {
             this.velocity.y = 0
+            this.position.y = 331
         } else {
             this.velocity.y += gravity;
         }
@@ -108,5 +109,38 @@ class Fighter extends Sprite {
         setTimeout (() => {
           this.isAttacking = false
         }, 100)
+    }
+
+    switchSprite(sprite){
+       switch(sprite){
+        case 'idle':
+            if(this.image !== this.sprites.idle.image){
+                this.image = this.sprites.idle.image
+                this.framesMax = this.sprites.idle.framesMax
+                this.frameCurrent = 0
+            }
+        break
+        case 'run':
+            if(this.image !== this.sprites.run.image){
+                this.image = this.sprites.run.image
+                this.framesMax = this.sprites.run.framesMax
+                this.frameCurrent = 0
+            }
+        break
+        case 'jump':
+            if(this.image !== this.sprites.jump.image){
+                this.image = this.sprites.jump.image
+                this.framesMax = this.sprites.jump.framesMax
+                this.frameCurrent = 0
+            }
+        break
+        case 'fall':
+            if(this.image !== this.sprites.fall.image){
+                this.image = this.sprites.fall.image
+                this.framesMax = this.sprites.fall.framesMax
+                this.frameCurrent = 0
+            }
+        break
+       }
     }
 }
