4e4bfc81a4659e7427675d0e9c4e20484220b4d8
Gabriel Baldez <130607246+GabrielBBaldez@users.noreply.github.com>
2024-10-28 20:35:20 -0300

    Upda Animation

--- index.js
+++ index.js
@@ -233,7 +233,9 @@ function animate() {
         ){
         enemy.takeHit()
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = enemy.health + `%`
+        gsap.to('#enemyHealth', {
+            width: enemy.health + `%`
+        })
     }
 
     // if player misses
@@ -249,7 +251,9 @@ function animate() {
         ){
         player.takeHit()
         enemy.isAttacking = false
-        document.querySelector('#playerHealth').style.width = player.health + `%`
+        gsap.to('#playerHealth', {
+            width: player.health + `%`
+        })
     }
 
     // if enemy misses
